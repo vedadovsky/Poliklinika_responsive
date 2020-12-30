b057959889b31c2cdb1fed5bde7af24b3c2cef35
vedadovsky <vedadovsky13@gmail.com>
2020-12-30 11:31:49 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+$('.slider-one').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+    nextArrow: ".site-slider .slider-btn .next",
+    prevArrow: ".site-slider .slider-btn .prev"
+});
